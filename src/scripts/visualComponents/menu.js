63b245ec95343f39e3ce269e47847923184a4655
samuelleaokenzie <samuel@kenzie.com.br>
2022-06-21 23:45:00 -0300

    first commit

---
+++ src/scripts/visualComponents/menu.js
@@ -0,0 +1,10 @@
+document.body.insertAdjacentHTML("afterbegin", `
+<nav class="menu bg-color-grey-4">
+    <ul class="container">
+        <li><a href="#">Início</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Objetivos</a></li>
+        <li><a href="#">Contatos</a></li>
+    </ul>
+</nav>`
+)
